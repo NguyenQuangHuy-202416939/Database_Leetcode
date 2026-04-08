591b8aafde44f0b792b161c0cbde124f20035944
Nguyễn Quang Huy <huybeo2908@gmail.com>
2026-04-08 19:35:02 +0700

    Time: 348 ms (99.47%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
+BEGIN
+    RETURN (
+        /* Write your T-SQL query statement below. */
+        SELECT TOP 1 salary FROM (
+            SELECT salary, DENSE_RANK() OVER ( ORDER BY salary DESC ) AS RANK FROM Employee  
+        ) AS ranking
+        WHERE RANK = @N
+    );
+END
