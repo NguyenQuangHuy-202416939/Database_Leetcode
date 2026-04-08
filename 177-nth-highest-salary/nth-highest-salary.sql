766f37364c3c1630615a2b91b1e774f32a0075a2
Nguyễn Quang Huy <huybeo2908@gmail.com>
2026-04-08 19:35:32 +0700

    Time: 403 ms (53.89%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
 BEGIN
     RETURN (
         /* Write your T-SQL query statement below. */
-        SELECT DISTINCT salary FROM (
+        SELECT TOP 1 salary FROM (
             SELECT salary, DENSE_RANK() OVER ( ORDER BY salary DESC ) AS RANK FROM Employee  
         ) AS ranking
         WHERE RANK = @N
